c9471542866670621cc816692fe0d51cd396d9b9
Michael Kwabena Kissi <rapcedy@gmail.com>
2023-12-06 15:21:23 +0000

    added Slidable widget to the Habit Tile

--- lib/components/habit_tile.dart
+++ lib/components/habit_tile.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class HabitTile extends StatelessWidget {
   final String habitName;
   final bool habitCompleted;
-  Function(bool?)? onChanged;
+  final Function(bool?)? onChanged;
+  final Function(BuildContext)? editTapped;
+  final Function(BuildContext)? deleteTapped;
 
-  HabitTile({
+
+  const HabitTile({
     super.key,
     required this.habitName,
     required this.habitCompleted,
     required this.onChanged,
+    required this.editTapped,
+    required this.deleteTapped,
   });
 
   @override
@@ -18,25 +24,46 @@ class HabitTile extends StatelessWidget {
     // final width = MediaQuery.of(context).size.width;
     return Padding(
       padding: EdgeInsets.all(height * 0.02),
-      child: Container(
-        padding: EdgeInsets.all(height * 0.015),
-        decoration: BoxDecoration(
-          color: Colors.cyan[300],
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Row(
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: const StretchMotion(),
           children: [
-            // checkbox
-            Checkbox(
-              value: habitCompleted,
-              onChanged: onChanged,
+            // edit
+            SlidableAction(
+              onPressed: editTapped,
+              backgroundColor: Colors.grey,
+              icon: Icons.edit,
+              borderRadius: BorderRadius.circular(10),
             ),
-            // habit name
-            Text(
-              habitName,
-              style: const TextStyle(fontSize: 16),
+            // delete
+            SlidableAction(
+              onPressed: deleteTapped,
+              backgroundColor: Colors.red,
+              icon: Icons.delete,
+              borderRadius: BorderRadius.circular(10),
             ),
           ],
+        ),
+        child: Container(
+          padding: EdgeInsets.all(height * 0.015),
+          decoration: BoxDecoration(
+            color: Colors.cyan[300],
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Row(
+            children: [
+              // checkbox
+              Checkbox(
+                value: habitCompleted,
+                onChanged: onChanged,
+              ),
+              // habit name
+              Text(
+                habitName,
+                style: const TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
         ),
       ),
     );
